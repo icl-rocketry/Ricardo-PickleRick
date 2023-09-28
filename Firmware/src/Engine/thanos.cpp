128a3a2ba1e4f75ef35a6c34ac5f4ac8843147d3
artemsheykin <artem.sheykin20@imperial.ac.uk>
2023-09-28 11:47:57 +0100

    thanos class

---
+++ Firmware/src/Engine/thanos.cpp
@@ -0,0 +1,130 @@
+#include "thanos.h"
+#include "Helpers/jsonconfighelper.h"
+
+Thanos::Thanos(uint8_t id, JsonObjectConst engineConfig, addNetworkCallbackFunction_t addNetworkCallbackFunction, RnpNetworkManager &networkmanager, uint8_t handlerServiceID) : 
+Engine(id, networkmanager, handlerServiceID)
+{
+    using namespace JsonConfigHelper;
+
+    auto engineConf = getIfContains<JsonObjectConst>(engineConfig, "engine");
+    _engine = std::make_unique<NetworkActuator>(0,
+                                                 getIfContains<uint8_t>(engineConf, "address"),
+                                                 handlerServiceID,
+                                                 getIfContains<uint8_t>(engineConf, "destination_service"),
+                                                 _networkmanager,
+                                                 getLogCB());
+                
+    addComponentNetworkCallback(_engine.get(),engineConf,addNetworkCallbackFunction);
+
+    auto igniterConf = getIfContains<JsonObjectConst>(engineConfig, "igniter");
+    _igniter = std::make_unique<NetworkActuator>(1,
+                                                 getIfContains<uint8_t>(igniterConf, "address"),
+                                                 handlerServiceID,
+                                                 getIfContains<uint8_t>(igniterConf, "destination_service"),
+                                                 _networkmanager,
+                                                 getLogCB());   
+
+    addComponentNetworkCallback(_igniter.get(),igniterConf,addNetworkCallbackFunction);
+
+    auto oxVentValveConf = getIfContains<JsonObjectConst>(engineConfig, "oxVentValve");
+    _oxVentValve = std::make_unique<NetworkActuator>(2,
+                                                getIfContains<uint8_t>(oxVentValveConf, "address"),
+                                                handlerServiceID,
+                                                getIfContains<uint8_t>(oxVentValveConf, "destination_service"),
+                                                _networkmanager,
+                                                getLogCB());
+
+    setIfContains<uint16_t>(oxVentValveConf,"closed_position",_oxVentValveClosed,false);
+    setIfContains<uint16_t>(oxVentValveConf,"open_position",_oxVentValveOpen,false);
+
+    addComponentNetworkCallback(_oxVentValve.get(),oxVentValveConf,addNetworkCallbackFunction);
+
+    auto fuelVentValveConf = getIfContains<JsonObjectConst>(engineConfig, "fuelVentValve");
+    _fuelVentValve = std::make_unique<NetworkActuator>(3,
+                                                getIfContains<uint8_t>(fuelVentValveConf, "address"),
+                                                handlerServiceID,
+                                                getIfContains<uint8_t>(fuelVentValveConf, "destination_service"),
+                                                _networkmanager,
+                                                getLogCB());
+
+    setIfContains<uint16_t>(fuelVentValveConf,"closed_position",_fuelVentValveClosed,false);
+    setIfContains<uint16_t>(fuelVentValveConf,"open_position",_fuelVentValveOpen,false);
+
+    addComponentNetworkCallback(_fuelVentValve.get(),fuelVentValveConf,addNetworkCallbackFunction);
+
+    auto fuelPrssValveConf = getIfContains<JsonObjectConst>(engineConfig, "fuelPrssValve");
+    _fuelPrssValve = std::make_unique<NetworkActuator>(4,
+                                                getIfContains<uint8_t>(fuelPrssValveConf, "address"),
+                                                handlerServiceID,
+                                                getIfContains<uint8_t>(fuelPrssValveConf, "destination_service"),
+                                                _networkmanager,
+                                                getLogCB());
+
+    setIfContains<uint16_t>(fuelPrssValveConf,"closed_position",_fuelPrssValveClosed,false);
+    setIfContains<uint16_t>(fuelPrssValveConf,"open_position",_fuelPrssValveOpen,false);
+
+    addComponentNetworkCallback(_fuelPrssValve.get(),fuelPrssValveConf,addNetworkCallbackFunction);
+}
+
+void Thanos::updateState()
+{
+    _engine->updateState();
+    _igniter->updateState();
+    _oxVentValve->updateState();
+    _fuelVentValve->updateState();
+    _fuelPrssValve->updateState();
+}
+
+void Thanos::execute(int32_t func)
+{
+    Engine::execute(func);
+}
+
+void Thanos::armEngine()
+{
+    _engine->arm();
+    _igniter->arm();
+    _oxVentValve->arm();
+    _fuelVentValve->arm();
+    _fuelPrssValve->arm();
+}
+
+void Thanos::shutdown()
+{
+    _engine->execute(shutdown_command_arg);
+    Engine::shutdown();
+    _state.runState = static_cast<uint8_t>(ENGINE_RUN_STATE::SHUTDOWN);
+}
+
+void Thanos::ignite()
+{
+    Engine::ignite();
+    _state.runState = static_cast<uint8_t>(ENGINE_RUN_STATE::IGNITION);
+    _engine->execute(ignition_command_arg);
+}
+
+void Thanos::update()
+{
+
+    if(_state.runState == static_cast<uint8_t>(ENGINE_RUN_STATE::SHUTDOWN)){
+        if(millis() - getStatePtr()->shutdownTime > vent_delay){
+            _oxVentValve->execute(_oxVentValveOpen);
+            _fuelVentValve->execute(_fuelVentValveOpen);
+            _fuelPrssValve->execute(_fuelPrssValveOpen);
+        } 
+    }
+}
+
+uint8_t Thanos::flightCheck()
+{
+    uint8_t res = 0;
+
+    res += _engine->flightCheck(_networkRetryInterval,_componentStateExpiry,"Engine:" + std::to_string(getID()));
+    res += _igniter->flightCheck(_networkRetryInterval,_componentStateExpiry,"Engine:" + std::to_string(getID()));
+    res += _oxVentValve->flightCheck(_networkRetryInterval,_componentStateExpiry,"Engine:" + std::to_string(getID()));
+    res += _fuelVentValve->flightCheck(_networkRetryInterval,_componentStateExpiry,"Engine:" + std::to_string(getID()));
+    res += _fuelPrssValve->flightCheck(_networkRetryInterval,_componentStateExpiry,"Engine:" + std::to_string(getID()));
+    
+    return res;
+    
+}
--- Firmware/src/Engine/thanos.h
+++ Firmware/src/Engine/thanos.h
@@ -1,9 +1,75 @@
 #pragma once
 
 #include "engine.h"
+#include "Helpers/jsonconfighelper.h"
+#include <librrc/networkactuator.h>
+
+
+struct ThanosState : public EngineState{};
 
 
 class Thanos : public Engine
 {
-    
+    public:
+
+        Thanos(uint8_t id, JsonObjectConst engineConfig, addNetworkCallbackFunction_t addNetworkCallbackFunction, RnpNetworkManager &networkmanager, uint8_t handlerServiceID);
+
+        void updateState() override;
+
+        uint8_t flightCheck() override;
+
+        void update() override;
+
+        void armEngine() override;
+
+        void execute(int32_t func) override;
+
+    private:
+
+        ThanosState _state;
+        EngineState *getStatePtr() override { return &_state; };
+
+        std::unique_ptr<NetworkActuator> _engine;
+
+        std::unique_ptr<NetworkActuator> _igniter;
+
+        std::unique_ptr<NetworkActuator> _oxVentValve;
+        uint16_t _oxVentValveClosed = 0;
+        uint16_t _oxVentValveOpen = 180;
+
+        std::unique_ptr<NetworkActuator> _fuelVentValve;
+        uint16_t _fuelVentValveClosed = 0;
+        uint16_t _fuelVentValveOpen = 180;
+
+        std::unique_ptr<NetworkActuator> _fuelPrssValve;
+        uint16_t _fuelPrssValveClosed = 0;
+        uint16_t _fuelPrssValveOpen = 180;
+
+        void ignite() override;
+        void shutdown() override;
+
+        static constexpr uint16_t _networkRetryInterval = 5000;
+        static constexpr uint16_t _componentStateExpiry = 1000;
+        static constexpr uint16_t _networkTimeout = 2000;
+
+        uint8_t ignition_command_arg = 1;
+        uint8_t shutdown_command_arg = 2;
+
+        uint64_t vent_delay = 1000;
+
+        //helper function to add component to network callback
+        template<typename T>
+        void addComponentNetworkCallback(T* component,JsonObjectConst componentconf,addNetworkCallbackFunction_t addNetworkCallbackFunction)
+        {
+            using namespace JsonConfigHelper;
+            addNetworkCallbackFunction(getIfContains<uint8_t>(componentconf,"address"),
+                                getIfContains<uint8_t>(componentconf,"destination_service"),
+                                [this,component](packetptr_t packetptr)
+                                    {
+                                        component->networkCallback(std::move(packetptr));
+                                    }
+                                ,
+                                true
+                                );
+        };
 };
