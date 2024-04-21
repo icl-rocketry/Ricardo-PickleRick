170fbeaffe07177e3f55026edf033e328da436b0
kiran <kd619@ic.ac.uk>
2024-04-22 00:59:09 +0100

    removed unused helpers and replaced all references to the local jsonconfighelper to the impelmentation in librrc

--- Firmware/src/Helpers/jsonconfighelper.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include <librrc/Helpers/jsonconfighelper.h>
-
-// namespace alais for legacy reasons. These helpers are now unified in librrc!
-namespace JsonConfigHelper = LIBRRC::JsonConfigHelper;
-
