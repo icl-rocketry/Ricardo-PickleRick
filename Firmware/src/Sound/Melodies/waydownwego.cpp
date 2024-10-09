8c698289ad061631000eca15487dbf70218bcb36
kiran <kd619@ic.ac.uk>
2024-10-09 14:57:37 +0100

    added new songs

---
+++ Firmware/src/Sound/Melodies/waydownwego.cpp
@@ -0,0 +1,73 @@
+#include "melodyLibrary.h" 
+#include "melodyClass.h"
+#include "pitches.h"
+
+
+ // Generated song file 
+ 
+ 
+melody_t<60> MelodyLibrary::waydownwego = { { { 
+{NOTE_F4,125},
+{NOTE_AS5,250},
+{NOTE_GS4,250},
+{NOTE_CS4,250},
+{REST,375},
+{NOTE_AS5,250},
+{NOTE_AS5,250},
+{NOTE_CS6,250},
+{NOTE_AS5,375},
+{NOTE_AS5,250},
+{NOTE_GS4,250},
+{NOTE_F4,250},
+{NOTE_GS4,250},
+{NOTE_AS5,250},
+{NOTE_CS6,250},
+{NOTE_AS5,375},
+{NOTE_AS5,250},
+{NOTE_GS4,250},
+{NOTE_F4,250},
+{NOTE_GS4,250}, 
+{NOTE_AS5,250},
+{NOTE_CS6,250},
+{NOTE_AS5,375},
+{NOTE_AS5,250},
+{NOTE_F5,375},
+{NOTE_DS5,125},
+{NOTE_CS5,125},
+{NOTE_AS5,250},
+{NOTE_AS5,375},
+{NOTE_AS5,250},
+{NOTE_GS4,250},
+{NOTE_F4,250},
+{NOTE_GS4,250},
+{NOTE_AS5,250},
+{NOTE_CS6,250},
+{NOTE_AS5,375},
+{NOTE_AS5,250},
+{NOTE_GS4,250},
+{NOTE_F4,250},
+{NOTE_GS4,250},
+{NOTE_AS5,250},
+{NOTE_CS6,250},
+{NOTE_AS5,375},
+{NOTE_AS5,250},
+{NOTE_GS4,250},
+{NOTE_F4,250},
+{NOTE_GS4,250},
+{NOTE_AS5,250},
+{NOTE_CS6,250},
+{NOTE_AS5,375},
+{NOTE_AS5,250},
+{NOTE_F5,375},
+{NOTE_DS5,125},
+{NOTE_CS5,125},
+{NOTE_AS5,250},
+{NOTE_AS5,250},
+{NOTE_F5,250},
+{NOTE_F5,375},
+{NOTE_F5,375},
+{REST,500}
+} }, 
+false 
+ };
+
