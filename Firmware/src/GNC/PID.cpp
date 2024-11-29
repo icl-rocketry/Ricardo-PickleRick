88dc6ea9753fb478a9084e3feb6611c6b5ab5e47
Ziyi Liu <ziyil.sz@icloud.com>
2024-11-29 13:32:06 +0000

    Added functions to find error and integral error

--- Firmware/src/GNC/PID.cpp
+++ Firmware/src/GNC/PID.cpp
@@ -22,6 +22,40 @@ void PID::createTestK_p() {
         0, 0, 0, 0;
 }
 
-void PID::Setup(){
-    createTestK_p();
+void PID::setup(){
+    createTestK_p(); 
+    setpoint << 1.0, 2.0, 3.0, 4.0, 5.0, 6.0;
+    previousError << 0.0, 0.0, 0.0, 0.0, 0.0, 0.0;
+    timestep = 0.01; 
+    integral_error_riemman << 0.0, 0.0, 0.0, 0.0, 0.0, 0.0;
+    integral_error_trapezoid << 0.0, 0.0, 0.0, 0.0, 0.0, 0.0;
 }
+
+void PID::updateErrors(Eigen::Matrix<float,1, 6> currentPosition){
+
+   Serial.println("Current position: " + String(currentPosition(0,3))); 
+
+    //STEP1: error = setpoint - inputMatrix; 
+    for (int i = 0; i < setpoint.cols(); i++) {
+        error (0,i) = setpoint(0,i) - currentPosition(0,i);
+        }
+   Serial.println("Error: " + String(error(0,3))); 
+
+    //STEP2: integral_error = 
+        //Righthand Riemman Sum
+    for (int i = 0; i < setpoint.cols(); i++) {
+        integral_error_riemman(0,i) += error(0,i)*timestep ; 
+        }
+   Serial.println("Int Riemman Error: " + String(integral_error_riemman(0,3))); 
+
+        //Trapezoid Rule
+    for (int i = 0; i < setpoint.cols(); i++) {
+        integral_error_trapezoid(0,i) += (error(0,i) + previousError(0,i))*timestep*0.5 ; 
+        }
+    Serial.println("Int Tra Error: " + String(integral_error_trapezoid(0,3))); 
+
+    previousError = error; 
+    Serial.println("Previous Error: " + String(previousError(0,3))); 
+
+}
+
--- Firmware/src/GNC/PID.h
+++ Firmware/src/GNC/PID.h
@@ -1,6 +1,7 @@
 #include <Eigen/Dense>
 #include "librrc/Remote/nrcremoteactuatorbase.h"
 #include <librnp/rnp_networkmanager.h>
+#include <Arduino.h>
 
 
 class PID : public NRCRemoteActuatorBase<PID>
@@ -10,8 +11,15 @@ class PID : public NRCRemoteActuatorBase<PID>
         NRCRemoteActuatorBase(networkmanager) 
         {};
         Eigen::Matrix<float,1, 4> outputMatrix(Eigen::Matrix<float,1, 6> inputMatrix);
-        void Setup();
+        void setup();
+        void updateErrors(Eigen::Matrix<float,1, 6> currentPosition);
     private:
         Eigen::Matrix<float,6, 4> K_p;
         void createTestK_p();
+        Eigen::Matrix<float,1, 6> setpoint; 
+        float timestep; 
+        Eigen::Matrix<float,1, 6> error;
+        Eigen::Matrix<float,1, 6> integral_error_riemman; 
+        Eigen::Matrix<float,1, 6> integral_error_trapezoid; 
+        Eigen::Matrix<float,1, 6> previousError; 
 };
