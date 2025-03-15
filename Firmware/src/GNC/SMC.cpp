d212227588cc5df7c8f4588694b3440b67997d5b
Raghav <raghav.ramkumar@icloud.com>
2025-03-15 15:42:13 +0000

    fixed smc

--- Firmware/src/GNC/SMC.cpp
+++ Firmware/src/GNC/SMC.cpp
@@ -2,7 +2,7 @@
 #include "SMC.h"
 #include<cmath>
 
-void PID::setup(){
+void LQR_SMC::setup(){
     createTestK_p(); 
     createTestK_i(); 
     createTestK_d(); //cpp is a sequencial language
@@ -14,20 +14,18 @@ void PID::setup(){
     m_previous_error << 0.0, 0.0, 0.0, 0.0, 0.0, 0.0;
     m_integral_error_riemman << 0.0, 0.0, 0.0, 0.0, 0.0, 0.0;
     m_integral_error_trapezoid << 0.0, 0.0, 0.0, 0.0, 0.0, 0.0;
-    armServos();
     m_previousSampleTime = millis();
 }
 
-void PID::update(Eigen::Matrix<float,1, 6> currentPosition){
+void LQR_SMC::update(Eigen::Matrix<float,1, 6> currentPosition){
     if (millis() - m_previousSampleTime >= m_actuationDelta) {
         updateErrors(currentPosition); 
         updateActuationValues(); 
-        // sendActuationCommands();
         m_previousSampleTime = millis();
     }
 }
 
-void PID::updateErrors(Eigen::Matrix<float,1, 6> currentPosition){
+void LQR_SMC::updateErrors(Eigen::Matrix<float,1, 6> currentPosition){
 
     // Serial.println("Current position: " + String(currentPosition(0,3))); 
 
@@ -60,12 +58,10 @@ void PID::updateErrors(Eigen::Matrix<float,1, 6> currentPosition){
     m_previous_error = m_error; 
 }
 
-void PID::smc(){
-    m_derivative_error;
-    m_error;
+void LQR_SMC::updateSMC(){
 
     //define sliding surface
-    Eigen::Matrix<float, 1, 3> s = m_derivative_error.block<1, 3>(0, 0) + lambda_smc.cwiseProduct(m_error.block<1, 3>(0, 0));
+    Eigen::Matrix<float, 1, 3> s = m_error.block<4, 6>(0, 0) + lambda_smc.cwiseProduct(m_error.block<1, 3>(0, 0));
     Eigen::Matrix<float, 1, 3> acceleration;
     SMC_x = -eta_x * tanh(sx / psi_x);
     acceleration << eta_smc(0,0)*tanh(s(0,0)/psi_smc(0,0)), eta_smc(0,1)*tanh(s(0,1)/psi_smc(0,1)), eta_smc(0,2)*tanh(s(0,2)/psi_smc(0,2));
@@ -76,23 +72,18 @@ void PID::smc(){
     double Ty = mass * acceleration(0,1);
     double alpha = asin(-Tx/T);
     double beta = asin(Ty/(T*cos(alpha)));
-    actuation_values_smc << T/2,T/2, alpha, beta;
+    m_smc_actuation_values << T/2,T/2, alpha, beta;
+
 
 }
 
-void PID::updateActuationValues(){
-    m_actuation_values = m_error * m_K_p + actuation_values_smc; // + K_i * integral_error_riemman + K_d * derivative_error; 
+void LQR_SMC::updateActuationValues(){
+
+    m_actuation_values = m_error * m_K_p + K_i * integral_error_riemman + m_smc_actuation_values;
     // Serial.println("Actuation Values: " + String(actuation_values(0,0)) + " " + String(actuation_values(0,1)) + " " + String(actuation_values(0,2)) + " " + String(actuation_values(0,3)));
 }
 
-void PID::sendActuationCommands() {
-    changeServoAngle(0,m_actuation_values(0,0)); // pitch servo
-    changeServoAngle(1,m_actuation_values(0,1)); // roll servo
-    // changePropPower(0,actuation_values(0,2)); 
-    // changePropPower(1,actuation_values(0,3)); 
-}
-
-void PID::createTestK_p() {
+void LQR_SMC::createTestK_p() {
     m_K_p << 0, 0, 0, 0,
     0, 0, 0, 0,
     0, 0, 0, 0,
@@ -101,7 +92,7 @@ void PID::createTestK_p() {
     0, 0, 0, 0;
 }
 
-void PID::createTestK_i() {
+void LQR_SMC::createTestK_i() {
     m_K_i << 0, 0, 0, 0,
     0, 0, 0, 0,
     0, 0, 0, 0,
@@ -110,31 +101,22 @@ void PID::createTestK_i() {
     0, 0, 0, 0;
 }
 
-void PID::createTestK_d() {
-    m_K_d << 0, 0, 0, 0,
-    0, 0, 0, 0,
-    0, 0, 0, 0,
-    0, 0, 0, 0,
-    0, 0, 0, 0,
-    0, 0, 0, 0;
-}
-
-void PID::createTestLambda() {
+void SMC::createTestLambda() {
     lambda_smc << 0.1, 0.1, 0.1;
 }
 
-void PID::createTestEta() {
+void SMC::createTestEta() {
     eta_smc << 10, 10, 0.1;
 }
 
-void PID::createTestPsi() {
+void SMC::createTestPsi() {
     psi_smc << 1, 1, 1;
 }
 
-void PID::calibrate_impl(packetptr_t packetptr) { // saves pid gains for all p, i & d matrices
-    PIDCalibrationPacket calibrate_comm(*packetptr);
+void SMC::calibrate_impl(packetptr_t packetptr) { // saves SMC gains for all p, i & d matrices
+    SMCCalibrationPacket calibrate_comm(*packetptr);
     std::vector<uint8_t> serializedData = packetptr->getBody();
-    //should be able to access k_11 k_12 from pidcalibpacket
+    //should be able to access k_11 k_12 from SMCcalibpacket
     //save that into the kp gains matrix
     std::string NVSName = "FRANCIS";
     NVSStore _NVS(NVSName, NVSStore::calibrationType::Servo);
@@ -146,7 +128,7 @@ void PID::calibrate_impl(packetptr_t packetptr) { // saves pid gains for all p, 
    
 };
 
-void PID::check_gains() {
+void SMC::check_gains() {
     std::string NVSName = "FRANCIS";
     NVSStore _NVS(NVSName, NVSStore::calibrationType::Servo);
     //load gains matrix and print it all to see if the calib command can change the values
@@ -157,7 +139,7 @@ void PID::check_gains() {
         Serial.println("no data") ;// default NC
         return;
     }
-    Serial.println("PID data:");
+    Serial.println("SMC data:");
     // setNormalState(calibpacket.normalState);
    // Display the matrix
     for (size_t j = 0; j < 4; ++j)
@@ -171,77 +153,10 @@ void PID::check_gains() {
     }
 }
 
-void PID::armServos() {
-    SimpleCommandPacket arm_alpha(3, 0); //3 here is the arm command
-    arm_alpha.header.source_service = 1;
-    arm_alpha.header.source = 2;
-    arm_alpha.header.destination_service = 10;
-    arm_alpha.header.destination = 102;
-    arm_alpha.header.uid = 0;
-    m_networkmanager.sendPacket(arm_alpha);
-    delay(100);
-    SimpleCommandPacket arm_beta(3, 0);
-    arm_beta.header.source_service = 1;
-    arm_beta.header.source = 2;
-    arm_beta.header.destination_service = 11;
-    arm_beta.header.destination = 102;
-    arm_beta.header.uid = 0;
-    m_networkmanager.sendPacket(arm_beta);
-}
-
-void PID::changeServoAngle(int servo, int angle) { // angle should be -10 to 10
-
-    angle = angle * 10; // scale the angle to 0.1 degree = 1 argument degree
-    uint8_t des_ser; 
-
-    if (servo == 0) { // mapped from 0 - 300 w 185 as 0
-        des_ser = 10; 
-        angle += 185;
-    }
-    if (servo == 1) { // mapped from 0 - 300 w 120 as 0
-        des_ser = 11; 
-        angle += 120;
-    }
-
-
-    angle = round(angle); //round to the nearest integer
-    Serial.println("Servo: " + String(servo) + ", Angle: " + String(angle));
-    SimpleCommandPacket actuate_servo(2, angle); //2 is the fire command
-    actuate_servo.header.source_service = 1;
-    actuate_servo.header.source = 2;
-    actuate_servo.header.destination_service = des_ser;
-    actuate_servo.header.destination = 102;
-    actuate_servo.header.uid = 0;
-    m_networkmanager.sendPacket(actuate_servo);
-}
-
-void PID::changePropPower(int prop, int power) {
-
-    uint8_t des_ser; 
-
-    if (prop == 0) {
-         des_ser = 10; 
-    }
-    if (prop == 1) {
-        des_ser = 11; 
-    }
-
-    //the power is already between 0 and 100 so no need to change
-
-    SimpleCommandPacket actuate_prop(2, power); //2 is the fire command
-    actuate_prop.header.source_service = 1;
-    actuate_prop.header.source = 2;
-    actuate_prop.header.destination_service = des_ser;
-    actuate_prop.header.destination = 103;
-    actuate_prop.header.uid = 0; //unknown
-    m_networkmanager.sendPacket(actuate_prop);
-
-}
-
-void PID::telemetry_impl(packetptr_t packetptr) {
+void SMC::telemetry_impl(packetptr_t packetptr) {
     SimpleCommandPacket packet(*packetptr);
 
-	PIDTelemetryPacket telemetry;
+	SMCTelemetryPacket telemetry;
 
 	telemetry.header.type = 108;
 	telemetry.header.source = packet.header.destination;
--- Firmware/src/GNC/SMC.h
+++ Firmware/src/GNC/SMC.h
@@ -3,17 +3,17 @@
 #include <librnp/rnp_networkmanager.h>
 
 #include <librrc/Helpers/nvsstore.h>
-#include "GNC/PIDCalibrationPacket.h"
-#include "GNC/PIDTelemetryPacket.h"
+#include "GNC/SMCCalibrationPacket.h"
+#include "GNC/SMCTelemetryPacket.h"
 #include "Config/services_config.h"
 #include <Arduino.h>
 
 
 
-class PID : public NRCRemoteControllerBase<PID>
+class LQR_SMC : public NRCRemoteControllerBase<LQR_SMC>
 {
     public:
-        PID(std::string name, Services::ID serviceID, RnpNetworkManager &networkmanager):
+        LQR_SMC(std::string name, Services::ID serviceID, RnpNetworkManager &networkmanager):
             NRCRemoteControllerBase(name, networkmanager), 
             m_networkmanager(networkmanager),
             m_serviceID(static_cast<uint8_t>(serviceID)) {};
@@ -22,8 +22,7 @@ class PID : public NRCRemoteControllerBase<PID>
         void setup();
         void update(Eigen::Matrix<float,1, 6> currentPosition);
         void check_gains();
-        void sendActuationCommands();
-        void PID::smc();
+        void updateSMC();
         float getThrust1(){return m_actuation_values(0,2);};
         float getThrust2(){return m_actuation_values(0,3);};
 
@@ -32,7 +31,6 @@ class PID : public NRCRemoteControllerBase<PID>
         uint8_t m_serviceID;
         Eigen::Matrix<float,6, 4> m_K_p;
         Eigen::Matrix<float,6, 4> m_K_i;
-        Eigen::Matrix<float,6, 4> m_K_d;
 
         Eigen::Matrix<float,1, 6> m_setpoint;
         float m_timestep; 
@@ -42,20 +40,15 @@ class PID : public NRCRemoteControllerBase<PID>
         Eigen::Matrix<float,1, 6> m_integral_error_riemman; 
         Eigen::Matrix<float,1, 6> m_integral_error_trapezoid; 
         Eigen::Matrix<float,1, 6> m_previous_error; //update this change later
-        Eigen::Matrix<float,1, 6> m_derivative_error; 
-        Eigen::Matrix<float,1, 4> m_actuation_values; 
-        Eigen::Matrix<float, 1, 4> actuation_values_smc
+        Eigen::Matrix<float,1, 4> m_actuation_values;
+        Eigen::Matrix<float, 1, 4> m_smc_actuation_values;
 
         void createTestK_p();
         void createTestK_i();
-        void createTestK_d();
         void createTestLambda();
         void createTestEta();
         void createTestPsi();
         void updateErrors(Eigen::Matrix<float,1, 6> currentPosition);
-        void armServos();
-        void changeServoAngle(int servo, int angle);
-        void changePropPower(int prop, int power) ;
 
         void calibrate_impl(packetptr_t packetptr);
         void telemetry_impl(packetptr_t packetptr);
@@ -64,7 +57,7 @@ class PID : public NRCRemoteControllerBase<PID>
         Egien::Matrix<float,1, 3> eta_smc;
         Eigen::Matrix<float,1, 3> psi_smc;
         
-        friend class NRCRemoteBase<PID>;
-        friend class NRCRemoteControllerBase<PID>;
+        friend class NRCRemoteBase<SMC>;
+        friend class NRCRemoteControllerBase<SMC>;
 
 };
